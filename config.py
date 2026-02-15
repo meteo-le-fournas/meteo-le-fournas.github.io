9ef8cbb4871fc59878e84d7975aec8d89cf962af
meteo-le-fournas <thibaud.godon@proton.me>
2026-02-15 21:09:13 +0100

    base html

---
+++ config.py
@@ -0,0 +1,9 @@
+# API documentation : https://doc.ecowitt.net/web/#/apiv3en?page_id=17
+API_URL = "https://api.ecowitt.net/api/v3/device/history"
+APPLICATION_KEY = "812B719EE0EC0F58631E65D6000F688E"
+API_KEY = "e477437e-c81c-4297-827c-b3d3d9c8c73d"
+MAC = "30:ED:A0:A6:CC:D0"
+DATA_FOLDER = "data"
+START_DATE = '2026-01-13' # station installed on 2026-01-13
+
+PAGE_TITLE = 'Meteo Le Fournas - Ferrals-les-Montagnes'
